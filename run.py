6b52eae9ff38a2b8d7d7f6dfdb8f78cddf1931de
Michael <bigapplematthias@gmail.com>
2023-01-12 17:05:11 +0100

    delete docker run python

--- run.py
+++ run.py
@@ -33,7 +33,9 @@ if not DEBUG:
     
 if DEBUG:
     app.logger.info('DEBUG       = ' + str(DEBUG)             )
+    
     app.logger.info('DBMS        = ' + app_config.SQLALCHEMY_DATABASE_URI)
+
     app.logger.info('ASSETS_ROOT = ' + app_config.ASSETS_ROOT )
 
 
