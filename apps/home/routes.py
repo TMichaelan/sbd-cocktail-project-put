9c2b6d29490883d5f15e3bc769e210c3708fbf50
Joramba <jorkabozko@gmail.com>
2023-01-12 23:12:26 +0100

    feat: add barman page

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -2,13 +2,13 @@
 """
 Copyright (c) 2019 - present AppSeed.us
 """
-
+import psycopg2
 from apps.home import blueprint
-from flask import render_template, request
+from flask import render_template, request, redirect
 from flask_login import login_required
 from jinja2 import TemplateNotFound
 
-from apps.costyl import costyl 
+from apps.costyl import costyl,get_barmans
 
 @blueprint.route('/index')
 @login_required
@@ -17,19 +17,73 @@ def index():
     return render_template('home/index.html', segment='index')
 
 
-# @blueprint.route('/coctails_cards')
+@blueprint.route('/coctails_cards.html',methods=('GET', 'POST'))
 # @login_required
-# def coctail():
-#     try:
-#         users = costyl()
-#         # Serve the file (if exists) from app/templates/home/FILE.html
-#         return render_template("home/coctails_cards.html", segment='coctails_cards', users= users)
+def coctails_cards():
 
-#     except TemplateNotFound:
-#         return render_template('home/page-404.html'), 404
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        github = request.form['github']
 
-#     except:
-#         return render_template('home/page-500.html'), 500
+        # print(username, email, password, github)
+
+        conn = psycopg2.connect('postgresql://joramba:admin@localhost:5432/bazy_danych')
+        cur = conn.cursor()
+        querry_add_user = 'INSERT INTO \"Users\" (username, email, password, oauth_github) VALUES (\'{}\', \'{}\', \'{}\',\'{}\');'.format(username, email, password, github)
+        cur.execute(querry_add_user)
+     
+        conn.commit()
+        cur.close()
+        conn.close()
+        return redirect('coctails_cards.html')
+
+
+    try:
+        users = costyl()
+        # Serve the file (if exists) from app/templates/home/FILE.html
+        segment = get_segment(request)
+        return render_template("home/coctails_cards.html" , segment=segment, users=users)
+    except TemplateNotFound:
+        return render_template('home/page-404.html'), 404
+
+    except:
+        return render_template('home/page-500.html'), 500
+
+
+
+@blueprint.route('/barman.html',methods=('GET', 'POST'))
+# @login_required
+def barman():
+    if request.method == 'POST':
+        name = request.form['name']
+        surname = request.form['surname']
+        phone = request.form['phone']
+        adres = request.form['adres']
+
+        # print(username, email, password, github)
+
+        conn = psycopg2.connect('postgresql://joramba:admin@localhost:5432/bazy_danych')
+        cur = conn.cursor()
+        querry_add_barman = 'INSERT INTO \"barman\" VALUES (\'{}\', \'{}\', \'{}\',\'{}\');'.format(name, surname, phone, adres)
+        cur.execute(querry_add_barman)
+     
+        conn.commit()
+        cur.close()
+        conn.close()
+        return redirect('barman.html')
+
+    try:
+        barmans = get_barmans()
+        # Serve the file (if exists) from app/templates/home/FILE.html
+        segment = get_segment(request)
+        return render_template("home/barman.html" , segment=segment, barmans=barmans)
+    except TemplateNotFound:
+        return render_template('home/page-404.html'), 404
+
+    except:
+        return render_template('home/page-500.html'), 500
 
 
 
@@ -38,32 +92,14 @@ def index():
 def route_template(template):
 
     try:
-
         if not template.endswith('.html'):
             template += '.html'
 
         # Detect the current page
-        segment = get_segment(request)
-        users = costyl()
-
-        # if request.method == 'POST':
-        #     username = request.form['username']
-        #     email = request.form['email']
-        #     password = request.form['password']
-        #     github = request.form['github']
-
-        #     conn = psycopg2.connect('postgresql://joramba:admin@localhost:5432/bazy_danych')
-        #     cur = conn.cursor()
-        #     cur.execute('INSERT INTO \"Users\"'
-        #                 'VALUES (%s, %s, %s, %s)',
-        #                 (username, email, password, github))
-        #     conn.commit()
-        #     cur.close()
-        #     conn.close()
-        #     return redirect('index')
+        segment = get_segment(request)        
         
         # Serve the file (if exists) from app/templates/home/FILE.html
-        return render_template("home/" + template, segment=segment, users= users)
+        return render_template("home/" + template, segment=segment)
 
     except TemplateNotFound:
         return render_template('home/page-404.html'), 404
